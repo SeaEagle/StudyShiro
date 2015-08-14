5e8b006b668d47c3f878ebd4049451d37bff5ecf
SeaEagle <gentlemaneagle@163.com>
2015-08-14 17:35:51 +0800

    自定义Realm
    自定义Realm

--- chapter02/src/test/java/ShiroLoginAndLogoutTest.java
+++ chapter02/src/test/java/ShiroLoginAndLogoutTest.java
@@ -13,7 +13,8 @@ import org.junit.Test;
 public class ShiroLoginAndLogoutTest {
 
     @Test
-    public void testLogin(){
+    public void testLoginAndLogout(){
+
         //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
         Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
         //2、得到SecurityManager实例 并绑定给SecurityUtils
@@ -37,5 +38,30 @@ public class ShiroLoginAndLogoutTest {
 
     }
 
+    @Test
+    public void testMyRealm(){
+
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-myrealm.ini");
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zheng", "123");
+
+        try{
+            //4、登录，即身份验证
+            subject.login(token);
+        }catch (AuthenticationException e){
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //5、断言用户已经登录
+
+        //6、退出
+        subject.logout();
+
+    }
 
 }
